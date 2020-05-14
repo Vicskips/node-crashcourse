0fbc17aeb6ddbe319c6750c62c104472c03008da
vicskips <vicskips@gmail.com>
2020-05-14 14:25:00 +0100

    Fixed server

--- index.js
+++ index.js
@@ -3,43 +3,9 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-//  if(req.url === "/"){
-//     fs.readFile(
-//     path.join(__dirname, "public", "index.html"),
-//     (err, content) => {
-//     if(err) throw err;
-//      res.writeHead(200, { "Content-Type": "text/html"});
-//      res.end(content);
-//  })
-// }
-
-
-// if(req.url === "/about"){
-//     fs.readFile(
-//     path.join(__dirname, "public", "about.html"),
-//     (err, content) => {
-//     if(err) throw err;
-//      res.writeHead(200, { "Content-Type": "text/html"});
-//      res.end(content);
-//  })
-// }
-
-
-// if(req.url === "/api/users"){
-//     const users = [
-//         {name: "Dave Williams", age: 40},
-//         {name: "Hen Vic", age: 30},
-//     ];
-//     res.writeHead(200, { "Content-Type": "application/json"});
-//     res.end(JSON.stringify(users));
-    
-// }
 
 //build file path
-let filePath = path.join(__dirname, "public", req.ur === "/" ? "index.html" :req.url );
-
-// console.log(filePath);
-// res.end();
+let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" :req.url );
 
 //extension of file
 let extname = path.extname(filePath);
@@ -90,4 +56,5 @@ fs.readFile(filePath, (err, content) =>{
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+
 
